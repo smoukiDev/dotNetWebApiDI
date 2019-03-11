237442fe8cf98ce59d7818fa11cb69a7019978de
Serhii Maksymchuk <smakdealcase@gmail.com>
2019-03-11 17:29:36 +0200

    Created simple codefirst model.

---
+++ dotNetWebApiDI/dotNetWebApiDI/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace dotNetWebApiDI.Models
+{
+    [Table("CUSTOMERS")]
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [MaxLength(255)]
+        public int FirstName { get; set; }
+    }
+}
---
+++ dotNetWebApiDI/dotNetWebApiDI/Persistance/ShopDbContext.cs
@@ -0,0 +1,37 @@
+namespace dotNetWebApiDI
+{
+    using dotNetWebApiDI.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.ModelConfiguration.Configuration;
+    using System.Linq;
+
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext(string connectionName)
+            : base(connectionName)
+        {
+        }
+
+        public virtual DbSet<Customer> MyEntities { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Customer>()
+                .HasKey(e => e.Id);
+
+            modelBuilder
+                .Entity<Customer>()
+                .Property(e => e.Id)
+                .HasColumnName("ID");
+
+            modelBuilder
+                .Entity<Customer>()
+                .Property(e => e.FirstName)
+                .HasColumnName("FIRSTNAME");
+        }
+    }
+
+
+}
