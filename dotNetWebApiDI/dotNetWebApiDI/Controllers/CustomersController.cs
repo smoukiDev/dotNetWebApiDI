4f37dd888e32bac8d437d53b5d2bbe8ee236e1cb
Serhii Maksymchuk <smakdealcase@gmail.com>
2019-03-11 17:55:52 +0200

    Implemented Customers controller without dependency injection.

---
+++ dotNetWebApiDI/dotNetWebApiDI/Controllers/CustomersController.cs
@@ -0,0 +1,52 @@
+﻿using dotNetWebApiDI.Core;
+using dotNetWebApiDI.Models;
+using dotNetWebApiDI.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace dotNetWebApiDI.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        private readonly IRepository<Customer> repository;
+
+        public CustomersController()
+        {
+            this.repository = new CustomesRepository();
+        }
+        // GET api/<controller>
+        public async Task<IHttpActionResult> Get()
+        {
+            var result = await this.repository.GetAllAsync();
+            return this.Ok(result);
+        }
+
+        // POST api/<controller>
+        public async Task<IHttpActionResult> Post([FromBody]Customer customer)
+        {
+            var result = await this.repository.AddAsync(customer);
+            return this.Created("/api/customers", result);
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
