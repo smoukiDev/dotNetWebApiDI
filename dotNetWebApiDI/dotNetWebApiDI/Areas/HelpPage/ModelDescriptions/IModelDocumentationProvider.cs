571fd2750b9be771b16d812222d26c3f8e0f5c63
Serhii Maksymchuk <smakdealcase@gmail.com>
2019-03-11 16:31:23 +0200

    Added WebApi project.

---
+++ dotNetWebApiDI/dotNetWebApiDI/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace dotNetWebApiDI.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ dotNetWebApiDI/dotNetWebApiDI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace dotNetWebApiDI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
